1c0ca318ca138a64ce87a182570dc0773645e562
Jack Brown <jack.brown.255@gmail.com>
2016-11-26 19:30:41 -0600

    Added read/write lock

--- incrementers/readwritelock_incrementer.c
+++ incrementers/readwritelock_incrementer.c
@@ -1,15 +1,21 @@
 #pragma once
 
+#include <pthread>
+
 extern static volatile unsigned int glob;
 
-void init_readwritelock(){
+pthread_rwlock_t rwlock;
 
+void init_readwritelock(){
+	pthread_rwlock_init(&rwlock, NULL);
 }
 
 void* increment_with_readwritelock(void* arg){
 	int numLoops = *((int *) arg);
 	while(numLoops-- > 0){
-		
+		pthread_rwlock_wrlock(&wrlock);
+		glob++;
+		pthread_rwlock_unlock(&wrlock);
 	}
 	return NULL;
 }
